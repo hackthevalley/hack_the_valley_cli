0b86cba1e03dc18b754827cff7a9279493ec858f
Jun Zheng <me@jackzh.com>
2018-12-11 04:23:02 -0500

    feature: Add api-ver and hackers subcommand structure

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,27 @@ extern crate clap;
 use clap::App;
 
 fn main() {
+
+    // Initialize CLI
     let yaml = load_yaml!("cli.yml");
-    let _matches = App::from_yaml(yaml).get_matches();
+    let matches = App::from_yaml(yaml).get_matches();
+
+    // Unwrap arguments
+    let url = matches.value_of("url").unwrap_or("https://api.hackthevalley.io");
+    let key = matches.value_of("key");
+    println!("Running command on endpoint: {}", url);
+    match key {
+        Some(key) => println!("Using key: {}", key),
+        None => println!("Not using key.")
+    }
+
+    // Decide which subcommand to run
+    if let Some(matches) = matches.subcommand_matches("api-ver") {
+        println!("api-ver...");
+    }
+
+    if let Some(matches) = matches.subcommand_matches("hackers") {
+        println!("hackers...");
+    }
+
 }
